febca4397c1ff66ac67654ffacc54af4a487bbae
ksteph10 <144149449+ksteph10@users.noreply.github.com>
2023-11-22 16:24:41 -0500

    lesson4.3

--- js/index.js
+++ js/index.js
@@ -26,3 +26,39 @@ for (let i = 0; i < skills.length; i++) {
     skillsSection.appendChild(skill);
 
 }
+     
+const messageForm = document.getElementById('leave_message');
+messageForm.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const userName = event.target.querySelector('[name="usersName"]').value;
+    const userEmail = event.target.querySelector('[name="usersEmail"]').value;
+    const userMessage = event.target.querySelector('[name="usersMessage"]').value;
+
+    console.log('Name:', userName);
+    console.log('Email:', userEmail);
+    console.log('Message:', userMessage);
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage= document.createElement('li');
+    newMessage.innerHTML =`<a href="mailto:${userEmail}">${userName}</a><span>${userMessage}</span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.type = 'button';
+
+    
+    removeButton.addEventListener('click', function(event) {
+        const entry = event.target.parentNode;
+        entry.remove();
+    });
+
+    messageList.append(newMessage);
+    newMessage.appendChild(removeButton);
+    event.target.reset();
+
+
+   
+});
+    
